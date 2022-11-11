2ebcaefc76360dd8368fe83c19ff97faf17013ff
Luis Reyes <luisdavid007@gmail.com>
2022-11-11 13:51:18 +0100

    Modularized main test

--- data/main.py
+++ data/main.py
@@ -1,6 +1,8 @@
 from threed_future_dataset import ThreedFutureDataset
 import matplotlib.pyplot as plt
+import os
 
-dataset = ThreedFutureDataset(root="/Users/luisreyes/Downloads/3D-FUTURE-model")
+root = os.environ.get("FUTURE_DATASET")
+dataset = ThreedFutureDataset(root=root)
 
 angel_voxel = dataset[10].show()
