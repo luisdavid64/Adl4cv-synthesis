af31c41ceb4ac9b1bb1720f78bfda809f9223de4
Luis Reyes <luisdavid007@gmail.com>
2022-11-23 01:26:43 +0100

    Improved assert message

--- test/pickled_dataset_tests.py
+++ test/pickled_dataset_tests.py
@@ -8,9 +8,8 @@ srcdir = '../data'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 import threed_future_dataset
 
-
 # Check that pickled dataset is 32x32x32
-dataset, stats = threed_future_dataset.ThreedFutureDataset.from_pickled_dataset("/tmp/threed_future.pkl", "/Users/luisreyes/Courses/ADL4CV/Project/data/output/dataset_stats.json")
+dataset, stats = threed_future_dataset.ThreedFutureDataset.from_pickled_dataset("/tmp/threed_future.pkl", "../data/output/dataset_stats.json")
 for i in range(len(dataset)):
-    assert np.array_equal(dataset[i]["matrix"].shape, np.array([32,32,32])), "The voxel grid is not 32x32x32."
+    assert np.array_equal(dataset[i]["matrix"].shape, np.array([32,32,32])), "Some data is not 32x32x32."
 print("Success: Dataset is in the right format.")
