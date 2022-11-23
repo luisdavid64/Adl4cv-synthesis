b16242b8da7a7b87eb7ee1485229e4bb1d7e9eb5
Luis Reyes <luisdavid007@gmail.com>
2022-11-23 02:12:11 +0100

    Typos

--- test/pickled_dataset_tests.py
+++ test/pickled_dataset_tests.py
@@ -15,10 +15,10 @@ for i in range(len(dataset)):
     assert np.array_equal(dataset[i]["matrix"].shape, np.array([32,32,32])), "Some data is not 32x32x32."
 print("Success: Dataset is in the right format.")
 
-assert stats["n_objects"] == len(dataset), "mismatch in number of objects"
-print("Success: Number of objects preserved after pickling")
+assert stats["n_objects"] == len(dataset), "mismatch in number of objects."
+print("Success: Number of objects preserved after pickling.")
 
 labels = threed_future_labels.THREED_FUTURE_LABELS.values()
 for i in range(len(dataset)):
-    assert  dataset[i]["label"] in labels, "A wrong label was found"
-print("Success: All data is correctly labelled")
+    assert  dataset[i]["label"] in labels, "A wrong label was found."
+print("Success: All data is correctly labelled.")
