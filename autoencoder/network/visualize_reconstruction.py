8a09714b390f1312367565ecaa75ce0852275041
Alexander Fuchs <fuchsale@in.tum.de>
2022-12-23 17:26:54 +0100

    added shape code preprocessing

--- autoencoder/network/visualize_reconstruction.py
+++ autoencoder/network/visualize_reconstruction.py
@@ -5,7 +5,7 @@ from autoencoder import Autoencoder
 import torch
 import trimesh
 from matplotlib import pyplot as plt
-
+import random
 # Marching cubes reconstruction of matrix for sanity check
 def marching_cubes(voxel_matrix):
     voxel_matrix = torch.round(voxel_matrix)
@@ -15,13 +15,13 @@ def marching_cubes(voxel_matrix):
 
 def main(hparams):
     autoencoder = Autoencoder(hparams)
-    autoencoder.load_state_dict(torch.load(hparams.pretrained_model_path))
+    autoencoder.load_state_dict(torch.load(hparams["pretrained_model_path"]))
     autoencoder.freeze()
-    with open(hparams.data_root, "rb") as f:
+    with open(hparams["data_root"], "rb") as f:
         dataset = pickle.load(f)
         dataset = list(map(lambda x: torch.from_numpy(x["matrix"]).float().unsqueeze(0), dataset))
-        k = 0 
-        my_sample = next(itertools.islice(dataset, k, k+1))
+        k = random.randint(0, len(dataset) - 1)
+        my_sample = dataset[k] #next(itertools.islice(dataset, k, k+1))
         x = autoencoder(torch.unsqueeze(my_sample,0))
         # Plot first example
         marching_cubes(x[0][0])
@@ -29,11 +29,12 @@ def main(hparams):
 if __name__ == "__main__":
     parser = ArgumentParser()
 
-    parser.add_argument("--data_root", type=str, default="/tmp/threed_future.pkl", help="Data root directory")
+    parser.add_argument("--data_root", type=str, default="../tmp/threed_future.pkl", help="Data root directory")
     parser.add_argument("--z_dim", type=int, default=128, help="Size of latent vector z")
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate for optimizer")
     parser.add_argument("--beta1", type=float, default=0.9, help="Beta1 hyperparameter for Adam optimizer")
     parser.add_argument("--beta2", type=float, default=0.999, help="Beta2 hyperparameter for Adam optimizer")
     parser.add_argument("--pretrained_model_path", type=str, default="output/pretrained_ae.pt", help="Pretrained model location")
-    args = parser.parse_args()
+    args = vars(parser.parse_args())
+    print(args)
     main(args)
